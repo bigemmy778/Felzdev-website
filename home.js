03e6fe038fdc841625f579cba5a2d58a84a27adc
bigemmy <149758550+bigemmy778@users.noreply.github.com>
2024-11-23 09:47:36 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,25 @@
+const buttonElement = document.querySelector('.Data-button');
+const applyButton = document.querySelector('.apply-btn');
+
+
+
+
+buttonElement.addEventListener('click', () => {
+    buttonElement.innerHTML = 'Loading...';
+}) 
+
+
+applyButton.addEventListener('click', () => {
+    applyButton.innerHTML = 'applying....'
+
+})
+
+
+
+
+
+const object = {
+
+}
+
+
